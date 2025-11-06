38be2423e2b980aec7a0eb324bd969f9ce0c348e
Lahmar22 <lahmarzakariae22@gmail.com>
2025-11-06 23:06:47 +0100

    add function operation

--- js/cartes.js
+++ js/cartes.js
@@ -8,6 +8,16 @@ export function visibleMoney() {
         money1.textContent = isHidden ? '****' : '10000 MAD';
     });
 }
+export function visibleMoney2() {
+    const toggleBtn2 = document.getElementById('btnVisible2');
+    const money2 = document.getElementById('money2');
+    let isHidden2 = true;
+
+    toggleBtn2.addEventListener('click', () => {
+        isHidden2 = !isHidden2;
+        money2.textContent = isHidden2 ? '****' : '10000 MAD';
+    });
+}
 
 
 
--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@ import { dashboardMenu } from './dashboard.js';
 import { headerMenu } from './dashboard.js';
 import { sidebar } from './dashboard.js';
 import { visibleMoney } from './cartes.js';
+import { visibleMoney2 } from './cartes.js';
 
 dashboardMenu();
 headerMenu();
 sidebar();
 visibleMoney();
+visibleMoney2()
 
 
 
