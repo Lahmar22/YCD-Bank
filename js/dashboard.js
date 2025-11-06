aa9271e3a1460e60a074cb7390d10922f523aa66
Lahmar22 <lahmarzakariae22@gmail.com>
2025-11-06 15:39:30 +0100

    add header and design

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,7 @@ export function dashboardMenu() {
     const btn = document.getElementById("dropdownBtn");
     const menu = document.getElementById("dropdownMenu");
 
-    
+
     if (!btn || !menu) {
         console.warn("Dropdown elements not found (check IDs: dropdownBtn, dropdownMenu)");
         return;
@@ -16,6 +16,22 @@ export function dashboardMenu() {
     document.addEventListener("click", (e) => {
         if (!btn.contains(e.target) && !menu.contains(e.target)) {
             menu.classList.add("hidden");
+        }
+    });
+}
+
+export function headerMenu() {
+    const notifBtn = document.getElementById('notifBtn');
+    const notifMenu = document.getElementById('notifMenu');
+
+    notifBtn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        notifMenu.classList.toggle('hidden');
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!notifBtn.contains(e.target) && !notifMenu.contains(e.target)) {
+            notifMenu.classList.add('hidden');
         }
     });
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { dashboardMenu } from './dashboard.js';
+import { headerMenu } from './dashboard.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   dashboardMenu();
+  headerMenu();
 
   function loadPage(page) {
     fetch(`${page}.html`)
