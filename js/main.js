b018e9f2ba7f92f473b8b01664af9194c2fc119b
Lahmar22 <lahmarzakariae22@gmail.com>
2025-11-06 15:54:56 +0100

    delete function hiding

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
 import { dashboardMenu } from './dashboard.js';
 import { headerMenu } from './dashboard.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-  dashboardMenu();
-  headerMenu();
-
-  function loadPage(page) {
-    fetch(`${page}.html`)
-      .then(res => res.text())
-      .then(data => document.getElementById("dashboard").innerHTML = data);
-  }
-
-  loadPage("dashboard");
-});
+dashboardMenu();
+headerMenu();
