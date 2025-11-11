660d192c064d72561d9eb99aac50c632ee1cecdf
Lahmar22 <lahmarzakariae22@gmail.com>
2025-11-11 16:12:43 +0100

    feat: update liste the beneficiere and design

--- js/beneficiere.js
+++ js/beneficiere.js
@@ -1,43 +1,63 @@
 
 const userData = [
-    { nom: "zaki", Rib: "876947592837401837491784" },
-    { nom: "lahmar", Rib: "876947592837401837491784" },
-    { nom: "ayoub", Rib: "876947592837401837491784" }
+    { id:1, nom: "Lahmar Zakariae", Rib: "876947592837401837491784" },
+    { id:2, nom: "Lahmar Zakariae", Rib: "876947592837401837491784" },
+    { id:3, nom: "Lahmar Zakariae", Rib: "876947592837401837491784" }
 ];
 
+let jsonUserData = JSON.stringify(userData);
 
-const tableBody = document.getElementById('tableBeneficier');
+localStorage.setItem("userData", jsonUserData);
 
-tableBody.className='divide-y divide-gray-100 text-gray-800';
 
-userData.forEach(personne => {
-    const row = tableBody.insertRow();
-    
-    let cellNom = row.insertCell();
-    cellNom.textContent = personne.nom;
-    cellNom.className = 'px-6 py-3';
+const listeBeneficier = document.getElementById('listeBeneficier');
 
-    let cellRIB = row.insertCell();
-    cellRIB.textContent = personne.Rib;
-    cellRIB.className = 'px-6 py-3';
+listeBeneficier.className = 'divide-y divide-gray-100 text-gray-800';
+const data = localStorage.getItem("userData");
 
-    let cellActions = row.insertCell();
-    cellActions.className = 'px-6 py-3';
-
-    const htmlContent = `
-        <div class="flex gap-3 justify-center items-center">
-            <button data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button" >
-                Modifier
-            </button>
-
-            <button class="block text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" type="button">
-                supprimer
-            </button>
-        </div>
+userData.forEach( (data) => {
+    const li = document.createElement("li");
+    li.className = 'flex flex-col md:flex-row md:justify-between md:items-center p-4 mb-3 rounded-lg shadow-lg bg-white border-0 gap-4';
+    li.innerHTML = `<div class="flex-1 flex flex-col sm:flex-row sm:gap-6">
         
+        <div class="flex-1">
+            <span class="font-bold sm:hidden">Nom: </span>
+            <span class="text-gray-800">${data.nom}</span>
+        </div>
+
+        <div class="flex-1">
+            <span class="font-bold sm:hidden">RIB: </span>
+            <span class="text-gray-800">${data.Rib}</span>
+        </div>
+    </div>
+
+    <div class="flex flex-col sm:flex-row gap-3 justify-end items-center mt-4 md:mt-0">
+        
+        <button 
+            data-modal-target="default-modal" 
+            data-modal-toggle="default-modal" 
+            class="block w-full sm:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" 
+            type="button"
+            aria-label="Modifier le bénéficiaire">
+            Modifier
+        </button>
+
+        <button 
+            class="block w-full sm:w-auto text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" 
+            type="button"
+            aria-label="Supprimer le bénéficiaire" onclick="supprimer(${data.id})">
+            Supprimer
+        </button>
+
+    </div>
     `;
+    listeBeneficier.appendChild(li);
 
-    cellActions.innerHTML = htmlContent;
 
-    
 });
+
+
+function supprimer(id){
+    if (!confirm("Voulez-vous vraiment supprimer ce versement ?")) return;
+    
+}
