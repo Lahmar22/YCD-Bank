db53e69f13869fc64013c066be1636948b5f77db
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2025-11-11 15:23:36 +0100

    delete app.js folder and fix bugs in payment.js

--- js/Payment.js
+++ js/Payment.js
@@ -3,6 +3,68 @@ const submitbtn_edit = document.getElementById("submitbtn_edit");
 const openPopup = document.getElementById("openPopup");
 const popupEdit = document.getElementById("popup_edit");
 const user_inside = document.getElementById("user_inside");
+const select = document.getElementById("select_which");
+const users_payment = document.getElementById("users_payment");
+const Reference = document.getElementById("Reference");
+const validatePayment = document.getElementById("validatePayment");
+
+
+
+select.addEventListener("change", function () {
+    const selectedValue = this.value;
+    if (selectedValue === "inwi" || selectedValue === "maroc-telecom" || selectedValue === "orange") {
+        users_payment.classList.remove("hidden");
+    }
+    else {
+        users_payment.classList.add("hidden")
+    }
+});
+
+validatePayment.addEventListener("click", () => {
+    if (select.value == "inwi" || select.value == "maroc-telecom" || select.value == "orange") {
+        if (!select.value) {
+            alert("Please select a compte");
+            return;
+        }
+
+        if (!user_inside.value) {
+            alert("Please select a user");
+            return;
+        }
+    }
+        const Reference_value = Number(Reference.value);
+        if (!Reference_value) {
+            alert("Please enter a valid amount");
+            return;
+        }
+
+    if (Reference_value > 10000) {
+        alert("You can't do more than 10,000 dhs");
+        return;
+    }
+
+    const select_value = select.value;
+
+
+    const paymentInfo = {
+        Reference_value,
+        select_value,
+    };
+
+
+    let Infos = JSON.parse(localStorage.getItem("Infos")) || [];
+    Infos.push(paymentInfo);
+    localStorage.setItem("Infos", JSON.stringify(Infos));
+
+
+
+    alert("Payment accepted");
+    select.value = "";
+    user_inside.value = "";
+    Reference.value = "";
+    users_payment.classList.add("hidden");
+
+});
 
 window.addEventListener("DOMContentLoaded", () => {
     const saved = localStorage.getItem("userPayment");
