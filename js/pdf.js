e7a5309c14ac0f6137480bb73622d5bea73fff02
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2025-11-11 12:52:00 +0100

    feat(pdf):add export button to export html to pdf

--- js/pdf.js
+++ js/pdf.js
@@ -3,7 +3,15 @@ const IbanBankID = document.getElementById("IbanBankID");
 const CodeBank = document.getElementById("CodeBank");
 const SubmitBtnPDF = document.getElementById("SubmitBtnPDF");
 const UserPDF = JSON.parse(localStorage.getItem("users")) || [];
+const bodyy = document.querySelector("#PDFContent");
 
-NomPrenom.placeholder = UserPDF[0].nom +" " + UserPDF[0].prenom;
-IbanBankID.placeholder = UserPDF[0].id;
-CodeBank.placeholder = UserPDF[0].transfer;
+NomPrenom.value = UserPDF[0].nom + " " + UserPDF[0].prenom;
+
+IbanBankID.value = UserPDF[0].id;
+CodeBank.value = UserPDF[0].transfer;
+SubmitBtnPDF.addEventListener("click", () => {
+    SubmitBtnPDF.style.display = "none";
+    html2pdf().from(bodyy).save().then(() => {
+        SubmitBtnPDF.style.display = "block";
+    });
+});
