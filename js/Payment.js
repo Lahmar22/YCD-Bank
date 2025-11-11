639c0de6545d51e932be5949df1e9da8ebcf3d03
ElMehdiCherkaoui <cherkaouim451@gmail.com>
2025-11-11 10:17:53 +0100

    feat(payment):add script to check number if exist and stop user

--- js/Payment.js
+++ js/Payment.js
@@ -20,6 +20,21 @@ submitbtn_edit.addEventListener("click", () => {
     const newName = document.getElementById("newName").value;
     const newNumber = document.getElementById("newNumber").value;
 
+    let numbers = JSON.parse(localStorage.getItem("numbers")) || [];
+    
+    for (let i = 0; i < numbers.length; i++) {
+        if (newNumber == numbers[i]) {
+            alert("this number already exist");
+            return;
+        }
+    }
+
+
+    numbers.push(newNumber);
+    localStorage.setItem("numbers", JSON.stringify(numbers));
+
+
+
     if (newName == "" || newNumber == "") {
         alert("invalid please fill the instructure or exit");
     }
@@ -29,7 +44,6 @@ submitbtn_edit.addEventListener("click", () => {
     }
     user_inside.innerHTML += `<option>${newName} - ${newNumber}</option>`;
     localStorage.setItem("userPayment", user_inside.innerHTML);
-
     popupEdit.classList.add("hidden");
 
 })
